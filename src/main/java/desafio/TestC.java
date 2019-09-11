46b11254ac9ab63cba6cd2fb00610862f33989b9
eduardotorres <dudutorres00@outlook.com>
2019-09-11 15:28:06 -0300

    teste no google

--- src/main/java/desafio/TestC.java
+++ src/main/java/desafio/TestC.java
@@ -1,7 +1,9 @@
 package desafio;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestC {
@@ -13,6 +15,14 @@ public class TestC {
 	WebDriver driver = new ChromeDriver();
 	driver.manage().window().maximize();
 	
+	driver.get("https://www.google.com/");
+	
+	WebElement pesquisa = driver.findElement(By.xpath("//*[@id='tsf']/div[2]/div[1]/div[1]/div/div[2]/input"));
+	pesquisa.clear();
+	pesquisa.sendKeys("Death Metal");
+	
+	WebElement pesquisar = driver.findElement(By.xpath("//*[@id='tsf']/div[2]/div[1]/div[3]/center/input[1]"));
+	pesquisar.click();
 	}
 
 }
